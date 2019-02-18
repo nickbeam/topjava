79b170bcd4e39f9a74bc67c67ed0b577698df689
nickbeam <dvininnick@gmail.com>
2019-02-18 14:06:41 +0300

    HW2 - Refactoring methods delete, get, getAll at InMemoryMealRepositoryImpl class

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -26,6 +26,10 @@ public class Meal extends AbstractBaseEntity {
         this.calories = calories;
     }
 
+    public Integer getUserId() {
+        return userId;
+    }
+
     public LocalDateTime getDateTime() {
         return dateTime;
     }
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepositoryImpl.java
@@ -3,11 +3,14 @@ package ru.javawebinar.topjava.repository.inmemory;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
 public class InMemoryMealRepositoryImpl implements MealRepository {
     private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
@@ -29,18 +32,23 @@ public class InMemoryMealRepositoryImpl implements MealRepository {
     }
 
     @Override
-    public void delete(int id) {
-        repository.remove(id);
+    public boolean delete(int id) {
+        return repository.remove(id) != null;
     }
 
     @Override
     public Meal get(int id) {
-        return repository.get(id);
+        return repository.values().stream()
+                .filter(meal -> meal.getId().equals(id))
+                .filter(meal1 -> meal1.getUserId().equals(SecurityUtil.authUserId()))
+                .findFirst().orElse(null);
     }
 
     @Override
     public Collection<Meal> getAll() {
-        return repository.values();
+        return repository.values().stream()
+                .filter(meal -> meal.getUserId().equals(SecurityUtil.authUserId()))
+                .sorted(Comparator.comparing(Meal::getDate))
+                .collect(Collectors.toList());
     }
 }
-
