3e5b097eab94280bcf5da5455cc7bf9142b68c46
nickbeam <dvininnick@gmail.com>
2019-02-25 13:15:15 +0300

    HW03 Add meals to initDB.sql and populateDB.sql

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
@@ -7,12 +8,12 @@ CREATE SEQUENCE global_seq START WITH 100000;
 CREATE TABLE users
 (
   id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-  name             VARCHAR                 NOT NULL,
-  email            VARCHAR                 NOT NULL,
-  password         VARCHAR                 NOT NULL,
-  registered       TIMESTAMP DEFAULT now() NOT NULL,
-  enabled          BOOL DEFAULT TRUE       NOT NULL,
-  calories_per_day INTEGER DEFAULT 2000    NOT NULL
+  name             VARCHAR                           NOT NULL,
+  email            VARCHAR                           NOT NULL,
+  password         VARCHAR                           NOT NULL,
+  registered       TIMESTAMP           DEFAULT now() NOT NULL,
+  enabled          BOOL                DEFAULT TRUE  NOT NULL,
+  calories_per_day INTEGER             DEFAULT 2000  NOT NULL
 );
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
 
@@ -23,3 +24,18 @@ CREATE TABLE user_roles
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+  id          integer default nextval('global_seq') not null
+    constraint meals_pkey
+      primary key,
+  user_id     integer                                         not null
+    constraint meals_user_id_fkey
+      references users
+      on delete cascade,
+  date_time   timestamp                                       not null,
+  description varchar                                         not null,
+  calories    integer                                         not null
+);
+create unique index if not exists meals_id_uindex on meals (id);
