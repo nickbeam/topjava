3cdda425a7b65d321888e386a20a0601c6352fcd
Nikolai <dvininnick@gmail.com>
2019-03-26 15:01:20 +0300

    HW06 - Ready without Optional

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.topjava.web.meal;
+
+import ru.javawebinar.topjava.service.MealService;
+
+public class MealRestController extends AbstractMealController {
+    public MealRestController(MealService service) {
+        super(service);
+    }
+}
